2c79aaad0bd3a4b6d4f748cffd31cb673d01166f
Stanislav Dolgachov <stanislav_dolgachov@epam.com>
2024-05-19 19:56:08 +0300

    Add simple triangle render

--- main.js
+++ main.js
@@ -1,7 +1,67 @@
 import "./style.css";
 
-document.querySelector("#app").innerHTML = `
-  <div>
-    <h1>Hello, world!</h1>
-  </div>
-`;
+const canvas = document.querySelector(".main-canvas");
+/** @type {WebGLRenderingContext} */
+const gl = canvas.getContext("webgl");
+
+// 1. Set Coordinates
+const vertexData = [0, 1, 0, 1, -1, 0, -1, -1, 0];
+
+// 2. Create Buffer
+const buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, buffer); // Bind buffer to GL context
+
+// 3. Load Vertex Data to Buffer
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
+// gl.STATIC_DRAW - how often Vertex Data Changes
+
+// 4. Create Vertex Shader
+const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+const vertexAttributeName = 'position';
+
+gl.shaderSource(
+  vertexShader,
+  `
+attribute vec3 ${vertexAttributeName};
+void main() {
+  gl_Position = vec4(${vertexAttributeName}, 1);
+}
+`
+);
+
+gl.compileShader(vertexShader);
+
+// 5. Create Fragment Shader
+const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+gl.shaderSource(
+  fragmentShader,
+  `
+  void main() {
+    gl_FragColor = vec4(0, 1, 1, 1);
+  }
+`
+);
+
+gl.compileShader(fragmentShader);
+
+// 6. Create Program
+const program = gl.createProgram();
+gl.attachShader(program, vertexShader);
+gl.attachShader(program, fragmentShader);
+gl.linkProgram(program);
+
+// 7. Enable Vertex Attributes
+const positionLocation = gl.getAttribLocation(program, vertexAttributeName);
+gl.enableVertexAttribArray(positionLocation);
+gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
+gl.useProgram(program);
+gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+/*
+create program
+attach shaders to program
+
+enable vertex attributes
+
+draw
+*/
